2b48352a589a2c6fa9600c9f7a5ce91a79f1b231
mxhaupt <maximilian.haupt@burda.com>
2017-04-15 13:47:38 +0200

    Removed unused used statement.

--- src/Plugin/AdContextInterface.php
+++ src/Plugin/AdContextInterface.php
@@ -3,7 +3,6 @@
 namespace Drupal\ad_entity\Plugin;
 
 use Drupal\Component\Plugin\PluginInspectionInterface;
-use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\TypedData\Plugin\DataType\Map;
 
