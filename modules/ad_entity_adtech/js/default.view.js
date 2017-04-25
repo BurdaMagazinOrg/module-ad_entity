b566738599c359aff822e896feb621142bccc9f4
mxhaupt <maximilian.haupt@burda.com>
2017-04-25 16:08:02 +0200

    Added slot numbering and page load flag.

--- modules/ad_entity_adtech/js/default.view.js
+++ modules/ad_entity_adtech/js/default.view.js
@@ -11,12 +11,17 @@
 
   Drupal.ad_entity.viewHandlers = Drupal.ad_entity.viewHandlers || {};
 
-  Drupal.ad_entity.viewHandlers.adtech_default = {
+  Drupal.ad_entity.viewHandlers.adtech_default = Drupal.ad_entity.viewHandlers.adtech_default || {
     attach: function (containers, context, settings) {
       if (typeof window.atf_lib !== 'undefined') {
         var load_arguments = [];
+        var onPageLoad = true;
+        if (this.numberOfAds > 0) {
+          onPageLoad = false;
+        }
         for (var id in containers) {
           if (containers.hasOwnProperty(id)) {
+            this.numberOfAds++;
             var container = containers[id];
             var ad_tag = $('.adtech-factory-ad', container[0]);
             var argument = {element: ad_tag[0]};
@@ -24,13 +29,19 @@
             if (targeting) {
               argument.targeting = JSON.parse(targeting);
             }
+            else {
+              argument.targeting = {};
+            }
+            argument.targeting.slotNumber = this.numberOfAds;
+            argument.targeting.onPageLoad = onPageLoad;
             load_arguments.push(argument);
           }
         }
         window.atf_lib.load_tag(load_arguments);
       }
     },
-    detach: function (containers, context, settings) {}
+    detach: function (containers, context, settings) {},
+    numberOfAds: 0
   };
 
 }(jQuery, Drupal, window));
--- modules/ad_entity_dfp/js/default.view.js
+++ modules/ad_entity_dfp/js/default.view.js
@@ -14,18 +14,23 @@
   var googletag = window.googletag || {};
   googletag.cmd = googletag.cmd || [];
 
-  Drupal.ad_entity.viewHandlers.dfp_default = {
+  Drupal.ad_entity.viewHandlers.dfp_default = Drupal.ad_entity.viewHandlers.dfp_default || {
     attach: function (containers, context, settings) {
+      var onPageLoad = 'true';
+      if (this.numberOfAds > 0) {
+        onPageLoad = 'false';
+      }
       for (var id in containers) {
         if (containers.hasOwnProperty(id)) {
+          this.numberOfAds++;
           var container = containers[id];
           var ad_tag = $('.google-dfp-ad', container[0]);
-          this.defineAndDisplay(ad_tag);
+          this.defineAndDisplay(ad_tag, this.numberOfAds.toString(), onPageLoad);
         }
       }
     },
     detach: function (containers, context, settings) {},
-    defineAndDisplay: function (ad_tag) {
+    defineAndDisplay: function (ad_tag, slotNumber, onPageLoad) {
       googletag.cmd.push(function () {
         var ad_id = ad_tag.attr('id');
         var network_id = ad_tag.attr('data-dfp-network');
@@ -49,12 +54,15 @@
             }
           }
         }
+        slot.setTargeting('slotNumber', slotNumber);
+        slot.setTargeting('onPageLoad', onPageLoad);
 
         slot.addService(googletag.pubads());
         googletag.display(ad_id);
         googletag.pubads().refresh([slot]);
       });
-    }
+    },
+    numberOfAds: 0
   };
 
 }(jQuery, Drupal, window));
