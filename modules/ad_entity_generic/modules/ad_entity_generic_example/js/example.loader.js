d00efc9b9b5dff61866bcdd1f82c61dd03c84277
mxhaupt <maximilian.haupt@burda.com>
2018-11-26 14:23:02 +0100

    Extend documentation.

--- modules/ad_entity_generic/modules/ad_entity_generic_example/js/example.loader.js
+++ modules/ad_entity_generic/modules/ad_entity_generic_example/js/example.loader.js
@@ -5,18 +5,25 @@
  * Short description about the example: This implementation is assuming
  * to be loaded asynchronously. If it's a little late, it may check for
  * globally available queues (toLoad and toRemove), whether some tags
- * are already waiting. It takes care of loading further ads on its own,
- * by adding itself as a loading and on-removal handler.
+ * are already waiting (see [10]). It takes care of loading further ads on its
+ * own, by adding itself as a loading and on-removal handler (see [8]).
  */
 
 (function (adEntity) {
 
+  // [1] This callback represents your own load handler.
   var loadCallback = function (ad_tags) {
     // Should not use the logger on production.
     console.log('Load handler called. Items to load: ' + ad_tags.length);
-    // Use the shift method to empty the incoming ad_tags queue.
+    // [2] Every ad_tag, which belongs to your handler, should be moved
+    //     from the incoming ad_tags queue, e.g. via the shift() method.
+    // [3] It should be noted, that there can be other load handlers,
+    //     having their own callbacks, which empty the ad_tags array.
+    //     If you want to push the whole ad_tags content to elsewhere,
+    //     like an event listener or to another command queue,
+    //     you should push a flat copy of the ad_tags array.
     var ad_tag = ad_tags.shift();
-    // Every ad_tag consists of the following properties:
+    // [4] Every ad_tag consists of the following properties:
     // - id: The DOM element ID as string.
     // - el: The DOM element as object itself.
     // - name: The internal machine name of the ad tag as string.
@@ -41,30 +48,31 @@
     //         This is similar to jQuery's data callback.
     while (typeof ad_tag === 'object') {
       console.log('Loaded ad tag ' + ad_tag.name);
-      // Important - call this function once loading is done:
+      // [5] Important - call this function once loading is done:
       ad_tag.done(true, false);
 
       ad_tag = ad_tags.shift();
     }
   };
+  // [6] In case you need to do something when ads are being removed
+  //     from the DOM, you can do this withing this function.
   var removeCallback = function (ad_tags) {
-    // In case you need to do something when ads are being removed from the DOM,
-    // you can do this withing this function.
     console.log('On-removal handler called. Items to remove: ' + ad_tags.length);
   };
 
+  // [7] Properly initialize the generic object.
   adEntity.generic = adEntity.generic || {toLoad: [], toRemove: [], loadHandlers: [], removeHandlers: []};
 
-  // Put your callbacks on top of the handler lists,
-  // to be the first one working on the incoming queues.
-  // At the end of each handler list, a "queue" handler
-  // is available to put all the rest of non-shifted
-  // tag items to its globally available queue.
+  // [8] Put your callbacks on top of the handler lists,
+  //     to be the first one working on the incoming queues.
+  // [9] At the end of each handler list, a "queue" handler
+  //     is will put all the rest of not yet pulled ad_tag
+  //     items to the globally available queue toLoad and toRemove.
   adEntity.generic.loadHandlers.unshift({name: 'example_loader', callback: loadCallback});
   adEntity.generic.removeHandlers.unshift({name: 'example_on_removal', callback: removeCallback});
 
-  // Directly check on the globally available queues,
-  // whether some tags are already waiting.
+  // [10] Directly check on the globally available queues,
+  //      whether some tags are already waiting.
   loadCallback(adEntity.generic.toLoad);
   removeCallback(adEntity.generic.toRemove);
 
